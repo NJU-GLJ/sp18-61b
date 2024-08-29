316c162504b155431596aa2ffb6b5fe834e5b581
NJU-GLJ <3133323853@qq.com>
2024-08-29 09:17:51 +0800

    proj0 completed

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -55,7 +55,7 @@ public class NBody {
             StdDraw.pause(10);
         }
         StdOut.printf("%d\n", planets.length);
-        StdOut.printf("%.2e\n", radius);
+        StdOut.printf("%.2e\n", R);
         for (int i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                     planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
